ee01f51cd44fea77ec64afb2e6c2b35580582816
o569903289 <0280106pc0119@123.local>
2016-02-04 16:53:24 +0800

    1

--- GoogleAds/testGADJava/app/src/main/java/com/zzw/testgadjava/MainActivity.java
+++ GoogleAds/testGADJava/app/src/main/java/com/zzw/testgadjava/MainActivity.java
@@ -6,15 +6,19 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.InterstitialAd;
 
 public class MainActivity extends ActionBarActivity {
 
+    AdView mAdView;
     InterstitialAd mInterstitialAd;
+    private RelativeLayout mainContainer;
     private Button mNewGameButton;
 
     @Override
@@ -22,27 +26,59 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AdView mAdView = (AdView) findViewById(R.id.adView);
-        AdRequest adRequest = new AdRequest.Builder().build();
-        mAdView.loadAd(adRequest);
-
-
-        mInterstitialAd = new InterstitialAd(this);
-        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
-        requestNewInterstitial();
+        this.preloadAds();
+        showBanner();
 
         mNewGameButton = (Button) findViewById(R.id.newgame_button);
         mNewGameButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (mInterstitialAd.isLoaded()) {
-                    mInterstitialAd.show();
-                } else {
-                    requestNewInterstitial();
-                    //close event
-                }
+                showInterstitial();
             }
         });
+    }
+
+    public void showBanner()
+    {
+        mAdView.setVisibility(View.VISIBLE);
+    }
+
+    public void hideBanner()
+    {
+        mAdView.setVisibility(View.INVISIBLE);
+    }
+
+    public void showInterstitial()
+    {
+        if (mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        } else {
+            requestNewInterstitial();
+            //close event
+        }
+    }
+    public void preloadAds()
+    {
+        //banner
+        mAdView = new AdView(this);
+        mAdView.setAdSize(AdSize.SMART_BANNER);
+        mAdView.setAdUnitId("ca-app-pub-3940256099942544/6300978111");
+        AdRequest adRequest = new AdRequest.Builder().addTestDevice("CAF6DED7ACA88F7ED718A7D6B87F2C86").build();
+        mAdView.loadAd(adRequest);
+
+        mainContainer = new RelativeLayout(this);
+        RelativeLayout.LayoutParams mp = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
+        mp.addRule(RelativeLayout.CENTER_IN_PARENT);
+        addContentView(mainContainer, mp);
+        RelativeLayout.LayoutParams bp = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        bp.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+        mainContainer.addView(mAdView, bp);
+        mAdView.setVisibility(View.INVISIBLE);
+        //banner end
+
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+        requestNewInterstitial();
 
         mInterstitialAd.setAdListener(new AdListener() {
             @Override
